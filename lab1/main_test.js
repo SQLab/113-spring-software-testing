2c738d56a2fc44c447fd5ec1403548b157f5dbe7
SapphireLinXu <linsapphire@gmail.com>
2025-03-06 16:54:53 +0800

    complete lab1/ file at local

--- lab1/main_test.js
+++ lab1/main_test.js
@@ -12,21 +12,51 @@ test("Test MyClass's addStudent", () => {
     assert.strictEqual(myClass.students.length, 1);
     assert.strictEqual(myClass.students[0], student);
 
+    const newStudentId2 = myClass.addStudent('not a student');
+    assert.strictEqual(newStudentId2, -1);
+
     // throw new Error("Test not implemented");
 
 });
 
 test("Test MyClass's getStudentById", () => {
     // TODO
-    throw new Error("Test not implemented");
+    const myClass = new MyClass();
+    const student = new Student();
+    myClass.addStudent(student);
+    const studentById = myClass.getStudentById(0);
+    assert.strictEqual(studentById, student);
+
+    const studentById2 = myClass.getStudentById(-1);
+    assert.strictEqual(studentById2, null);
+    const studentById3 = myClass.getStudentById(1);
+    assert.strictEqual(studentById3, null);
+
+    // throw new Error("Test not implemented");
 });
 
 test("Test Student's setName", () => {
     // TODO
-    throw new Error("Test not implemented");
+    const student = new Student();
+    student.setName("John");
+    assert.strictEqual(student.getName(), "John");
+
+    const student2 = new Student();
+    student2.setName(123);
+    assert.strictEqual(student.getName(), "John");
+
+    // throw new Error("Test not implemented");
 });
 
 test("Test Student's getName", () => {
     // TODO
-    throw new Error("Test not implemented");
+    const student = new Student();
+    student.setName("John");
+    assert.strictEqual(student.getName(), "John");
+
+    const student2 = new Student();
+    student2.setName(123);
+    assert.strictEqual(student2.getName(), '');
+
+    // throw new Error("Test not implemented");
 });
