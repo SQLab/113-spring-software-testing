8fda5a017c64420cc9e2e20d9786443460c5cc54
SapphireLinXu <linsapphire@gmail.com>
2025-03-06 15:36:54 +0800

    unit test addStudent

--- lab1/main_test.js
+++ lab1/main_test.js
@@ -1,20 +1,28 @@
 const test = require('node:test');
 const assert = require('assert');
 const { MyClass, Student } = require('./main');
+const exp = require('constants');
 
 test("Test MyClass's addStudent", () => {
     // TODO
-    throw new Error("Test not implemented");
+    const myClass = new MyClass();
+    const student = new Student();
+    const newStudentId = myClass.addStudent(student);
+    assert.strictEqual(newStudentId, 0);
+    assert.strictEqual(myClass.students.length, 1);
+    assert.strictEqual(myClass.students[0], student);
+
+    // throw new Error("Test not implemented");
 });
 
 test("Test MyClass's getStudentById", () => {
     // TODO
-    throw new Error("Test not implemented");
+    // throw new Error("Test not implemented");
 });
 
 test("Test Student's setName", () => {
     // TODO
-    throw new Error("Test not implemented");
+    // throw new Error("Test not implemented");
 });
 
 test("Test Student's getName", () => {
