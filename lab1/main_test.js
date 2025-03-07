22a76d46c4242e68714a1711ed81e5f7ad865d43
NienTzu <nientzulin@NienTzudeMacBook-Pro.local>
2025-03-07 23:14:47 +0800

    Submit Lab1

--- lab1/main_test.js
+++ lab1/main_test.js
@@ -2,22 +2,61 @@ const test = require('node:test');
 const assert = require('assert');
 const { MyClass, Student } = require('./main');
 
+
+
 test("Test MyClass's addStudent", () => {
     // TODO
-    throw new Error("Test not implemented");
+    const myClass = new MyClass();
+
+    const student_0 = 123;
+    assert.strictEqual(myClass.addStudent(student_0), -1);
+
+    const student_1 = new Student();
+    student_1.setName('John');
+    assert.strictEqual(myClass.addStudent(student_1), 0);
+
+    // throw new Error("Test not implemented");
 });
 
 test("Test MyClass's getStudentById", () => {
     // TODO
-    throw new Error("Test not implemented");
+    const myClass = new MyClass();
+
+    assert.strictEqual(myClass.getStudentById(99), null);
+
+    const student_1 = new Student();
+    student_1.setName("John");
+    const studentID_1 = myClass.addStudent(student_1);
+    const retrievedStudent_1 = myClass.getStudentById(studentID_1);
+    assert.ok(retrievedStudent_1 instanceof Student, "Should return a Student object");
+    assert.strictEqual(myClass.getStudentById(studentID_1).getName(), "John");
+
+    // throw new Error("Test not implemented");
 });
 
 test("Test Student's setName", () => {
     // TODO
-    throw new Error("Test not implemented");
+    const student = new Student();
+    student.setName("John")
+    assert.strictEqual(student.getName(), "John");
+
+    student.setName(123);
+    assert.strictEqual(student.getName(), 'John');
+
+    // throw new Error("Test not implemented");
 });
 
 test("Test Student's getName", () => {
     // TODO
-    throw new Error("Test not implemented");
+    const student = new Student();
+
+    assert.strictEqual(student.getName(), '');
+
+    student.setName("John");
+    assert.strictEqual(student.getName(), "John");
+
+    student.setName(123);
+    assert.strictEqual(student.getName(), "John");
+
+    // throw new Error("Test not implemented");
 });
