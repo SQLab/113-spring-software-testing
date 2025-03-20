3d4032122edc06142f1e24b5c854ecd4ead9dc05
Annheij <ann.heijkenskjold@gmail.com>
2025-03-20 12:39:08 +0800

    lab2 completed

--- lab2/main.js
+++ lab2/main.js
@@ -28,18 +28,14 @@ class Application {
         this.people = [];
         this.selected = [];
         this.mailSystem = new MailSystem();
-        this.getNames().then(([people, selected]) => {
-            this.people = people;
-            this.selected = selected;
-        });
     }
 
     async getNames() {
         const data = await readFile('name_list.txt', 'utf8');
-        const people = data.split('\n');
-        const selected = [];
-        return [people, selected];
+        this.people = data.split('\n');
+        this.selected = [];
     }
+
 
     getRandomPerson() {
         const i = Math.floor(Math.random() * this.people.length);
