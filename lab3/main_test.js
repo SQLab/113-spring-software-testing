55be598b3f66d54e36aa387284e7728d2981d3c1
SapphireLinXu <linsapphire@gmail.com>
2025-03-20 15:31:36 +0800

    complete coverage 100%

--- lab3/main_test.js
+++ lab3/main_test.js
@@ -3,3 +3,21 @@ const assert = require('assert');
 const { Calculator } = require('./main');
 
 // TODO: write your tests here
+describe('Calculator', () => {
+    it('should calculate exp correctly', () => {
+        const calculator = new Calculator();
+        const infiniteValue = Infinity;
+        assert.throws(() => calculator.exp(infiniteValue), Error);
+        assert.throws(() => calculator.exp(Number.MAX_VALUE), Error);
+        assert.strictEqual(calculator.exp(1), Math.exp(1));
+    });
+
+    it('should calculate log correctly', () => {
+        const calculator = new Calculator();
+        const infiniteValue = Infinity;
+        assert.throws(() => calculator.log(infiniteValue), Error);
+        assert.strictEqual(calculator.log(2), Math.log(2));
+        assert.throws(() => calculator.log(0), Error);
+        assert.throws(() => calculator.log(-1), Error);
+    });
+});
