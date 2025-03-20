e5c17a45410e1bbead7076a4da128d0ea8f1e003
SapphireLinXu <linsapphire@gmail.com>
2025-03-20 15:47:13 +0800

    parameterized test

--- lab3/main_test.js
+++ lab3/main_test.js
@@ -4,12 +4,27 @@ const { Calculator } = require('./main');
 
 // TODO: write your tests here
 describe('Calculator', () => {
+    
     it('should calculate exp correctly', () => {
+        const testcases = [
+            [Infinity, 'unsupported operand type'],
+            [Number.MAX_VALUE, 'overflow'],
+            [1, Math.exp(1)],
+            // [2, Math.log(2)],
+            // [0, 'unsupported operand type'],
+            // [-1, 'unsupported operand type']
+        ];
         const calculator = new Calculator();
-        const infiniteValue = Infinity;
-        assert.throws(() => calculator.exp(infiniteValue), Error);
-        assert.throws(() => calculator.exp(Number.MAX_VALUE), Error);
-        assert.strictEqual(calculator.exp(1), Math.exp(1));
+        for (const [input, expected] of testcases) {
+            if (typeof expected === 'string') {
+                assert.throws(() => calculator.exp(input), Error);
+            } else {
+                assert.strictEqual(calculator.exp(input), expected);
+            }
+        }
+        // assert.throws(() => calculator.exp(infiniteValue), Error);
+        // assert.throws(() => calculator.exp(Number.MAX_VALUE), Error);
+        // assert.strictEqual(calculator.exp(1), Math.exp(1));
     });
 
     it('should calculate log correctly', () => {
