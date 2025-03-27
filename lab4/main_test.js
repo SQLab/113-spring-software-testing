d5284a308b4821aa21d06e15b0018ad2fc54694a
mamie1031 <sciencemamie@gmail.com>
2025-03-27 19:03:58 +0800

    feat: use puppteer to get title

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -10,13 +10,50 @@ const puppeteer = require('puppeteer');
 
     // Hints:
     // Click search button
+    const searchButton = 'button.DocSearch-Button';
+    await page.waitForSelector(searchButton);
+    await page.click(searchButton);
+    
+    
     // Type into search box
-    // Wait for search result
-    // Get the `Docs` result section
-    // Click on first result in `Docs` section
-    // Locate the title
-    // Print the title
+    const searchBox = 'input.DocSearch-Input'
+    await page.waitForSelector(searchBox);
+    await page.type(searchBox, 'andy popoo');
 
+    // Wait for search result
+    function delay(time) {
+        return new Promise(function(resolve) { 
+            setTimeout(resolve, time)
+        });
+    }
+    await delay(4000);
+
+    const docSearchHits = await page.$$('.DocSearch-Hits');
+    for (let hit of docSearchHits) {
+        const sourceElement = await hit.$('.DocSearch-Hit-source');
+        if (sourceElement) {
+            const textContent = await sourceElement.evaluate(el => el.textContent);
+
+            // Get the `Docs` result section
+            if (textContent && textContent.includes('ElementHandle')) {
+                
+                // Click on first result in `Docs` section
+                const firstLink = await hit.$('a');
+                if (firstLink) {
+                    await firstLink.click();
+                    break; 
+                }
+            }
+        }
+    }
+
+    // Locate the title
+    await page.waitForSelector('h1'); 
+    const title = await page.$eval('h1', h1 => h1.innerText); 
+  
+    // Print the title
+    console.log(title);
+    
     // Close the browser
     await browser.close();
 })();
