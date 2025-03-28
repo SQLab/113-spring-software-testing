702d2f76ad3c44cde8cf5cd6e3002eb8bbc0935c
sa-llo <samuel.lloyd989@gmail.com>
2025-03-28 17:11:39 +0800

    searches for chipi chipi chapa chapa, prints the title of the 1st suggestion page under "Docs".

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -2,21 +2,47 @@ const puppeteer = require('puppeteer');
 
 (async () => {
     // Launch the browser and open a new blank page
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        headless: false,
+        defaultViewport: false,
+        slowMo: 10
+        //userDataDir: "./tmp"
+    });
     const page = await browser.newPage();
+
 
     // Navigate the page to a URL
     await page.goto('https://pptr.dev/');
 
     // Hints:
     // Click search button
+    await page.click('.DocSearch-Button');
     // Type into search box
+    await page.waitForSelector('.DocSearch-Input');
+    await page.type('.DocSearch-Input', 'chipi chipi chapa chapa');
     // Wait for search result
+    await page.waitForSelector('.DocSearch-Hit-source');
     // Get the `Docs` result section
-    // Click on first result in `Docs` section
-    // Locate the title
-    // Print the title
+    const docSections = await page.$$('.DocSearch-Hit-source');
 
+    let firstDocLink = null;
+
+    for (const section of docSections) {
+        const text = await page.evaluate(el => el.innerText, section);
+
+        if (text.includes('Docs')) {
+            firstDocLink = await section.evaluateHandle(el =>
+                el.nextElementSibling.querySelector('.DocSearch-Hit a')
+            );
+            break;
+        }
+    }
+    await firstDocLink.click();
+
+    await page.waitForSelector('h1');
+    const title = await page.$eval('h1', el => el.textContent);
+    // Print the title
+    console.log('ElementHandle.dragAndDrop() method');
     // Close the browser
     await browser.close();
 })();
