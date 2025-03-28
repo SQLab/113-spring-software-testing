c02844aebd824e29aae9c7506a5f930d663039ad
NienTzu <nientzulin@NienTzudeMacBook-Pro.local>
2025-03-28 18:43:08 +0800

    Submit Lab4

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -10,13 +10,25 @@ const puppeteer = require('puppeteer');
 
     // Hints:
     // Click search button
+    const selectSearchButton = '#__docusaurus > nav > div.navbar__inner > div.navbar__items.navbar__items--right > div.navbarSearchContainer_IP3a > button';
+    await page.waitForSelector(selectSearchButton);
+    await page.click(selectSearchButton);
     // Type into search box
+    await page.waitForSelector('#docsearch-input');
+    await page.type('#docsearch-input', 'andy popoo');
     // Wait for search result
+    const searchResultSelect = '#docsearch-hits1-item-4 > a';
     // Get the `Docs` result section
+    await page.waitForSelector(searchResultSelect);
     // Click on first result in `Docs` section
+    await page.click(searchResultSelect)
     // Locate the title
-    // Print the title
-
-    // Close the browser
+    const textSelector = await page.waitForSelector(
+        '#__docusaurus_skipToContent_fallback > div > div > main > div > div > div > div > article > div.theme-doc-markdown.markdown > header > h1'
+    );
+    const fullTitle = await textSelector?.evaluate(el => el.textContent);
+    // // Print the title
+    console.log(fullTitle);
+    // // Close the browser
     await browser.close();
 })();
