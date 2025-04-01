1f6234a7cbdfd3441cb6174ba5ae3c563f7923f9
wen <haowen0905@gmail.com>
2025-04-01 17:16:39 +0800

    Add Puppeteer Ops.

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -8,25 +8,19 @@ const puppeteer = require('puppeteer');
     // Navigate the page to a URL
     await page.goto('https://pptr.dev/');
 
-    // Hints:
-    // Click search button
     await page.click('.DocSearch.DocSearch-Button');
     await page.waitForSelector('#docsearch-input', { visible: true });
-    // Type into search box
-    await page.type('#docsearch-input', 'Docs', { delay: 100 });
-    // Wait for search result
-    await page.waitForSelector('#docsearch-list', { visible: true, timeout: 20000 });
-    await new Promise(resolve => setTimeout(resolve, 1000));
-    // Get the Docs result section
-    // Click on first result in Docs section
-    await page.waitForSelector('#docsearch-hits0-item-0', { visible: true });
-    await page.click('#docsearch-hits0-item-0');
-    await page.waitForFunction('document.title === "WebDriver BiDi support | Puppeteer"',{ timeout: 30000 });
-    // Locate the title
-    const title = await page.title();
-    // Print the title
-    console.log('Page Title:', title);
 
-    // Close the browser
+    await page.type('#docsearch-input', 'andy popoo', { delay: 100 });
+
+    await page.waitForSelector('#docsearch-list', { visible: true, timeout: 20000 });
+
+    await page.click('#docsearch-hits1-item-4 a');
+
+    await page.waitForSelector('header h1', { visible: true, timeout: 30000 });
+
+    const headerContent = await page.$eval('header h1', element => element.textContent);
+    console.log(`${headerContent}`);
+
     await browser.close();
 })();
