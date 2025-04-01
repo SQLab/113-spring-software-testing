9c6ffd87ff37ca18c33b2e54a3c7a64ec9af5272
cfmc30 <tedted30@hotmail.com>
2025-04-01 17:47:40 +0800

    feat: lab4 ok

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -17,6 +17,20 @@ const puppeteer = require('puppeteer');
     // Locate the title
     // Print the title
 
+    await page.click('.DocSearch-Button');
+    const searchBoxSelector = '.DocSearch-Input';
+    await page.waitForSelector(searchBoxSelector);
+    await page.type(searchBoxSelector, 'andy popoo');
+
+    await page.locator('#docsearch-hits1-item-4 > a:nth-child(1) > div:nth-child(1)').click();
+    // await page.waitForSelector(`.theme-doc-markdown > header:nth-child(1) > h1:nth-child(1)').innerText`);
+    await new Promise(r => setTimeout(r, 1000));
+    title = await page.evaluate(`document.querySelector('.theme-doc-markdown > header:nth-child(1) > h1:nth-child(1)').innerText `);
+    console.log(title);
+    
+    await page.screenshot({path: 'screenshot.png'});
+    
+
     // Close the browser
     await browser.close();
 })();
