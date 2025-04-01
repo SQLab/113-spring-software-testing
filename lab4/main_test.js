7a09302cb34ed4d324d38d12eec1f6b32486dd62
wen <haowen0905@gmail.com>
2025-04-01 16:50:14 +0800

    Add test of main.js.

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -10,12 +10,22 @@ const puppeteer = require('puppeteer');
 
     // Hints:
     // Click search button
+    await page.click('.DocSearch.DocSearch-Button');
+    await page.waitForSelector('#docsearch-input', { visible: true });
     // Type into search box
+    await page.type('#docsearch-input', 'Docs', { delay: 100 });
     // Wait for search result
-    // Get the `Docs` result section
-    // Click on first result in `Docs` section
+    await page.waitForSelector('#docsearch-list', { visible: true, timeout: 20000 });
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    // Get the Docs result section
+    // Click on first result in Docs section
+    await page.waitForSelector('#docsearch-hits0-item-0', { visible: true });
+    await page.click('#docsearch-hits0-item-0');
+    await page.waitForFunction('document.title === "WebDriver BiDi support | Puppeteer"',{ timeout: 30000 });
     // Locate the title
+    const title = await page.title();
     // Print the title
+    console.log('Page Title:', title);
 
     // Close the browser
     await browser.close();
