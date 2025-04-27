085c3b0bc4b027a199c3244cab6ab0db9b47495c
nizw0 <me@nizw0.dev>
2025-04-27 15:02:00 +0800

    feat: implement antiasan to bypass ASAN

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,6 +1,13 @@
 #include <string.h>
 
-void antiasan(unsigned long addr)
-{
+void antiasan(unsigned long addr) {
+  unsigned long start = addr + 0x87;
+  unsigned long end = start + 0x87;
 
+  for (unsigned long p = start; p < end; p += 8) {
+    unsigned char *shadow = (unsigned char *)((p >> 3) + 0x7fff8000);
+    strcpy((char *)shadow, "\0");
+    // Or just set the shadow to 0
+    // *shadow = 0;
+  }
 }
