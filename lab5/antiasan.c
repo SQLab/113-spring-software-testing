cb8fec6f4df325b10e98ebd1b173b467b9d6b61e
sa-llo <samuel.lloyd989@gmail.com>
2025-04-27 17:15:54 +0800

    updated antiasan.c to complete the assigment requirements

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,6 +1,13 @@
+#include <dlfcn.h>
+#include <stddef.h>
 #include <string.h>
 
-void antiasan(unsigned long addr)
-{
-
+void antiasan(unsigned long addr) {
+    char *buffer = (char *)addr;
+    size_t length = strlen(buffer) + 1;
+    void (*unpoison)(const void *, size_t) =
+        (void (*)(const void *, size_t))dlsym(RTLD_DEFAULT, "__asan_unpoison_memory_region");
+    if (unpoison) {
+        unpoison(buffer, length);
+    }
 }
