a0e761cc5bd914a7048aed57ef5991cd3fbac380
mamie1031 <sciencemamie@gmail.com>
2025-04-27 14:02:19 +0800

    feat: bypass ASAN

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,6 +1,11 @@
-#include <string.h>
+#include <stdint.h>
 
-void antiasan(unsigned long addr)
-{
-
+void antiasan(unsigned long addr) {
+    unsigned long gBadBuf = addr;
+    unsigned long gS = gBadBuf + 0xC0; 
+    unsigned long shadow_base = 0x7fff8000;  // ASAN shadow memory offset
+    unsigned char *shadow_gS_end = (unsigned char *)((gS + sizeof(char) * 24) >> 3) + shadow_base; 
+    for (int i = 0; i < 5; i++) {
+        shadow_gS_end[i] = 0;
+    }
 }
