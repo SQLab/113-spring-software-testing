91133b38f3ab13393dc0a0ef1399c8087e8c00be
sa-llo <samuel.lloyd989@gmail.com>
2025-04-27 17:39:57 +0800

    changed file again, validate.sh gives me pass but not autograder in git...

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,11 +1,17 @@
-#include <stdint.h>
+
+ #include <stdint.h>
 
  void antiasan(unsigned long addr) {
-     unsigned long gBadBuf = addr;
-     unsigned long gS = gBadBuf + 0xC0;
-     unsigned long shadow_base = 0x7fff8000;  // ASAN shadow memory offset
-     unsigned char *shadow_gS_end = (unsigned char *)((gS + sizeof(char) * 24) >> 3) + shadow_base;
-     for (int i = 0; i < 5; i++) {
-         shadow_gS_end[i] = 0;
+     unsigned long bufStart   = addr;
+     unsigned long overflowSrc = bufStart + 0xC0;
+     unsigned long SHADOW_BASE = 0x7fff8000UL;
+     /* compute pointer to the shadow bytes covering the end of your 24-byte spill */
+     unsigned char *shadowPtr =
+         (unsigned char *)(((overflowSrc + 24) >> 3) + SHADOW_BASE);
+
+     /* clear the next 5 shadow‐bytes so ASan won’t flag them */
+     size_t count = 5;
+     while (count--) {
+         *shadowPtr++ = 0;
      }
  }
