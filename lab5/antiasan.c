a6a96d71a644639896440e323b1915750f52a624
sa-llo <samuel.lloyd989@gmail.com>
2025-04-27 17:25:22 +0800

    changed file again, validate.sh gives me pass but not autograder in git...

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <dlfcn.h>
 #include <stddef.h>
 #include <string.h>
@@ -5,9 +6,12 @@
 void antiasan(unsigned long addr) {
     char *buffer = (char *)addr;
     size_t length = strlen(buffer) + 1;
-    void (*unpoison)(const void *, size_t) =
-        (void (*)(const void *, size_t))dlsym(RTLD_DEFAULT, "__asan_unpoison_memory_region");
-    if (unpoison) {
-        unpoison(buffer, length);
+    void *handle = dlopen(NULL, RTLD_LAZY);
+    if (handle) {
+        void (*unpoison)(const void *, size_t) =
+            (void (*)(const void *, size_t))dlsym(handle, "__asan_unpoison_memory_region");
+        if (unpoison) {
+            unpoison(buffer, length);
+        }
     }
 }
