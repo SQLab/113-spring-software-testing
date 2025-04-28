fdea9a2cfdc4163a57a3217c264e023793023690
NienTzu <nientzulin@NienTzudeMacBook-Pro.local>
2025-04-28 16:17:38 +0800

    submitLab5_8

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -17,7 +17,7 @@ void antiasan(unsigned long addr)
     unsigned long start_addr = (addr - 0x100);
 
     unsigned char* shadow = (unsigned char *)((start_addr >> 3) + SHADOW_OFFSET);
-    for (int i = 0; i < 256; i++) {
+    for (int i = 0; i < 64; i++) {
         shadow[i] = 0;
     }
 }
