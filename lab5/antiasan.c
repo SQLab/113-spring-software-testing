bd2645409a74ea01961c89e14b7410f590e9725e
Ramez <ramri882@student.liu.se>
2025-04-28 14:12:37 +0800

    Implement antiasan to bypass ASan detection by clearing shadow memory

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -2,5 +2,11 @@
 
 void antiasan(unsigned long addr)
 {
+    unsigned long shadow_base = 0x7fff8000;
+    unsigned char *shadow = (unsigned char *)((addr >> 3) + shadow_base);
 
+    for (int i = 0; i < 64; i++) {
+        shadow[i] = 0;
+    }
 }
+
