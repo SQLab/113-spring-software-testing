ffc862ff12c4d7bfe4d6946c734acaa8288998fc
NienTzu <nientzulin@NienTzudeMacBook-Pro.local>
2025-04-28 15:50:07 +0800

    submitLab5_3

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -2,18 +2,20 @@
 #include <stdint.h>
 
 #ifdef __APPLE__
+
 void antiasan(unsigned long addr)
 {
     (void)addr;
 }
+
 #else
-// Linux 平台，真的清除 shadow memory
+
 void antiasan(unsigned long addr)
 {
     const unsigned long SHADOW_OFFSET = 0x7fff8000;
     unsigned char *shadow_addr = (unsigned char *)((addr >> 3) + SHADOW_OFFSET);
 
-    for (int i = -4; i < 20; i++) {
+    for (int i = -8; i < 32; i++) {
         shadow_addr[i] = 0;
     }
 }
