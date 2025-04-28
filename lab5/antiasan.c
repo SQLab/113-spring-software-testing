719e0d1398a4694cdad4299df700d77ffd8570df
NienTzu <nientzulin@NienTzudeMacBook-Pro.local>
2025-04-28 15:29:45 +0800

    lab5

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,6 +1,10 @@
 #include <string.h>
+#include <stdint.h>
 
 void antiasan(unsigned long addr)
 {
-
+    unsigned long shadow_idx = (addr >> 3) + 0x7fff8000;
+    for(int i = 0; i < 0x20; i++){
+        ((volatile char *)shadow_idx)[i] = 0;
+    }
 }
