506daad046c3fd219946d02c899dc493d402360a
BrianGodd(黃永恩) <82190198+BrianGodd@users.noreply.github.com>
2025-04-29 18:04:08 +0800

    Update antiasan.c with new changes

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -5,5 +5,5 @@ extern char gS[0x18]; //存取gs的位址
 
 void antiasan(unsigned long addr)
 {
-    __asan_unpoison_memory_region(gS, 0x30); // 24 + 14 + 1 = 39：清 0x27 ok，但多保留點潛在區域
+    __asan_unpoison_memory_region(gS, 0x32); // 24 + 14 + 1 = 39：清 0x27 ok，但多保留點潛在區域
 }
