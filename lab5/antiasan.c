69af752fbee0e5ffcbd96ee30a8d47530d5b2fa6
313505006 <ljx901024.ee13@nycu.edu.tw>
2025-04-29 15:06:07 +0800

    finished lab5

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,6 +1,13 @@
-#include <string.h>
+// antiasan.c
+#include <sanitizer/asan_interface.h>
+#include "antiasan.h"
+
+// 明確告訴編譯器大小
+extern char gS[0x18];
+extern char gBadBuf[0x87];
 
 void antiasan(unsigned long addr)
 {
-
+    __asan_unpoison_memory_region((void *)addr,          0x87);
+    __asan_unpoison_memory_region(gS + 0x18,              0x10);
 }
