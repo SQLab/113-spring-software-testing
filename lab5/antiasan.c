2da32c10e41a2363fbffdd0ddf18c55b5c75c7a3
Bruce <bruce6005@gmail.com>
2025-04-29 14:55:08 +0800

    lab5

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,6 +1,18 @@
-#include <string.h>
 
-void antiasan(unsigned long addr)
-{
+#include <string.h>
+#include <stdlib.h>
+void antiasan(unsigned long addr) {
+    const unsigned long SHADOW_OFFSET = 0x7fff8000;
+
+    unsigned long start_addr = (addr - 0x100);
+
+    unsigned char* shadow = (unsigned char *)((addr >> 3) + SHADOW_OFFSET);
+    for (int i = 0; i < 0x100; i++) {
+        shadow[i] = 0;
+    }
+
+
 
 }
+
+
