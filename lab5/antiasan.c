722ece529f1398c884e9ab8f5c7ceb231d38eaab
jayhsiao <shjay629@gmail.com>
2025-04-29 16:29:22 +0800

    Fix unpoison region size to cover full overflow range

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -3,6 +3,6 @@
 extern char gS[0x18];
 
 void antiasan(unsigned long addr) {
-    // 從 gS 開始解毒，共解毒 0x18 (gS) + 0x08 (overflow)
-    __asan_unpoison_memory_region(gS, 0x18 + 0x08);
+    // 解毒 gS + 最大 15 bytes 溢位區間，保險設為 0x28
+    __asan_unpoison_memory_region(gS, 0x28);
 }
