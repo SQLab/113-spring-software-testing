39d15e96e549de3156e17646d6ae0947b716cfae
9ithu6-c0m <x@example.invalid>
2025-04-29 21:05:00 +0800

    Complete lab5

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,6 +1,11 @@
-#include <string.h>
+static inline unsigned long get_shadow_mem_addr(unsigned long addr) {
+    return ((addr) >> 3) + 0x7FFF8000;
+}
 
-void antiasan(unsigned long addr)
-{
-
+void antiasan(unsigned long addr) {
+    for (int i = 0; i < 0x10; i += 2) {
+        unsigned long target = addr + 0xc0 + 0x17 + i;
+        *(char *)get_shadow_mem_addr(target) = 0;
+        *(char *)get_shadow_mem_addr(target + 1) = 0;
+    }
 }
