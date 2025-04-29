db3e0d306d7b8f69c883a89acdf267dc15519345
cfmc30 <145142703+cfmc30@users.noreply.github.com>
2025-04-29 21:23:47 +0800

    fix: remove unused outputs

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -4,12 +4,9 @@
 
 void antiasan(unsigned long addr)
 {
-  printf("addr: %p\n", addr);
-  // memset((char*)addr, 0, 320);
   unsigned long s_addr = (addr >> 3) + 0x7FFF8000;
   unsigned long off = 0;
   for (int i = 0; i < 240; i++){
-    /* printf("fill: %p\n", s_addr + off + i); */
      *((char*)(s_addr + off + i)) = 0;
   }
 }
