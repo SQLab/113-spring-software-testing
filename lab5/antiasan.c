f429cd5ba0fe33eec255416ec5f25c715b42f678
cfmc30 <145142703+cfmc30@users.noreply.github.com>
2025-04-29 21:18:59 +0800

    feat: lab5

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,6 +1,15 @@
 #include <string.h>
+#include <stdio.h>
+
 
 void antiasan(unsigned long addr)
 {
-
+  printf("addr: %p\n", addr);
+  // memset((char*)addr, 0, 320);
+  unsigned long s_addr = (addr >> 3) + 0x7FFF8000;
+  unsigned long off = 0;
+  for (int i = 0; i < 240; i++){
+    /* printf("fill: %p\n", s_addr + off + i); */
+     *((char*)(s_addr + off + i)) = 0;
+  }
 }
