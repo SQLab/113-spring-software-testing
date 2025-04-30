e0759a49bd85e21db13114eb21e25bbb296cc90e
sapphirelinxu <linsapphire@gmail.com>
2025-04-30 22:22:59 +0800

    complete lab5

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,6 +1,9 @@
-#include <string.h>
+#include <sanitizer/asan_interface.h>
 
+//TODO:
 void antiasan(unsigned long addr)
 {
-
+    // __asan_unpoison_memory_region((void *)addr, 64);
+    // __asan_unpoison_memory_region((void *)addr, 128);
+    __asan_unpoison_memory_region((void *)addr, 256);
 }
