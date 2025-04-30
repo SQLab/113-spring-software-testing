e92f1baed45671aa82cbe1ff5dd5151a81775158
資安小菜🐤 <181969527+p0tat0ba11@users.noreply.github.com>
2025-04-30 15:40:29 +0000

    feat(lab5): bypass detection of ASan in

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,6 +1,4 @@
-#include <string.h>
-
-void antiasan(unsigned long addr)
-{
-
+#include <sanitizer/asan_interface.h>
+void antiasan(unsigned long addr) {
+    __asan_unpoison_memory_region((void *)addr, 0x100);
 }
