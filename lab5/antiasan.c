48de44ba27df3dd77e21132adca1a57720b18acd
unknown <dragon41012@gmail.com>
2025-05-01 14:48:56 +0800

    Lab5: 完成 antiasan 解毒 redzone 實作

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,6 +1,11 @@
-#include <string.h>
+// antiasan.c
+#include "antiasan.h"
+#include <sanitizer/asan_interface.h>
 
 void antiasan(unsigned long addr)
 {
+    void *start = (void*)(addr - 0x20);
+    size_t len   = 0x200;      // 解毒 0x200 bytes（512 bytes），足夠覆蓋所有紅區
 
+    __asan_unpoison_memory_region(start, len);
 }
