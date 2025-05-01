e8d963ac15278c4e2517456879a064c99ce0a909
Chen Chun-Hsuan <hsuan@Hsuans-MacBook-Air.local>
2025-05-01 10:50:49 +0800

    fix: antiasan.c

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,7 +1,9 @@
 #include <sanitizer/asan_interface.h>
 #include <string.h>
 
+extern char gS[0x18];
+
 void antiasan(unsigned long addr)
 {
-    __asan_unpoison_memory_region((void *) addr, 0x30);
+    __asan_unpoison_memory_region(gS, 0x30);
 }
