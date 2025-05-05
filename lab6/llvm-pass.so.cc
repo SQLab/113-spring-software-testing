06de040488cb8cc3ccce9dc2aa388b05de1deeff
sa-llo <samuel.lloyd989@gmail.com>
2025-05-05 15:40:58 +0800

    completed all three requirements. Commented code. All code written inside for-loop and inside the if-statement looking for the main function.

--- lab6/llvm-pass.so.cc
+++ lab6/llvm-pass.so.cc
@@ -1,12 +1,6 @@
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/IR/IRBuilder.h"
-#include "llvm/IR/Module.h"
-#include "llvm/IR/Constants.h"
-#include "llvm/IR/Function.h"
-#include "llvm/IR/Type.h"
-#include "llvm/IR/Value.h"
-#include "llvm/Support/raw_ostream.h"
 
 using namespace llvm;
 
@@ -17,32 +11,34 @@ struct LLVMPass : public PassInfoMixin<LLVMPass> {
 PreservedAnalyses LLVMPass::run(Module &M, ModuleAnalysisManager &MAM) {
   LLVMContext &Ctx = M.getContext();
   IntegerType *Int32Ty = IntegerType::getInt32Ty(Ctx);
-  PointerType *CharPtrTy = Type::getInt8PtrTy(Ctx);
-  PointerType *CharPtrPtrTy = PointerType::getUnqual(CharPtrTy);
-
-  // Get reference to debug(int) function
-  FunctionCallee debug_func = M.getOrInsertFunction("debug", Type::getVoidTy(Ctx), Int32Ty);
+  FunctionCallee debug_func = M.getOrInsertFunction("debug", Int32Ty);
   ConstantInt *debug_arg = ConstantInt::get(Int32Ty, 48763);
 
+
+
   for (auto &F : M) {
-    errs() << "func: " << F.getName() << "\n";
-    if (F.getName() == "main") {
-      IRBuilder<> Builder(&*F.getEntryBlock().getFirstInsertionPt());
 
-      // Call debug(48763);
-      Builder.CreateCall(debug_func, {debug_arg});
 
-      // argc = 48763
-      Argument *argcArg = F.getArg(0);
-      argcArg->replaceAllUsesWith(debug_arg);
 
-      // argv[1] = "hayaku... motohayaku!"
-      Argument *argvArg = F.getArg(1);
-      Value *index1 = ConstantInt::get(Int32Ty, 1);
-      Value *argv1_ptr = Builder.CreateInBoundsGEP(CharPtrTy, argvArg, index1);
-      Value *hayakuStr = Builder.CreateGlobalStringPtr("hayaku... motohayaku!");
-      Builder.CreateStore(hayakuStr, argv1_ptr);
+ // Find main function, call the debug function with argument 48763
+
+    if (F.getName() == "main"){
+    IRBuilder<> Builder(&*F.getEntryBlock().getFirstInsertionPt());
+    Builder.CreateCall(debug_func. {debug_arg});
+ // Overwrote argv1 with string "hayaku... motohayaku!"
+    Argument *argvArg = F.getArg(1);
+    Value index1 = ConstantInt::get(Int32Ty, 1);
+    Value *argv1_ptr = Builder.CreateInBound_GEP(CharPtrTy, argvArg, index1);
+    Value *hayakuStr = Builder.CreateGlobalStringPtr("hayaku... motohayaku!");
+    Builder.CreateStore(hayakuStr, argv1_ptr);
+ // Overwrote argcArg with 48763
+    Argument *argcArg = F.getArg(0);
+    argcArg -> replaceAllUsesWith(debug_arg);
     }
+
+
+
+
   }
   return PreservedAnalyses::none();
 }
@@ -57,3 +53,4 @@ llvmGetPassPluginInfo() {
         });
     }};
 }
+
