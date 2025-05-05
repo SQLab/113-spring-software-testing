59a6fcc0c1d2c1ac26bb7ce36c90fb8bf5407747
sa-llo <samuel.lloyd989@gmail.com>
2025-05-05 14:42:22 +0800

    testing third try of solution - modification of llvm-pass.so.cc. 100% complete

--- lab6/llvm-pass.so.cc
+++ lab6/llvm-pass.so.cc
@@ -1,47 +1,57 @@
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Type.h"
+#include "llvm/IR/Value.h"
+#include "llvm/Support/raw_ostream.h"
+
 using namespace llvm;
+
 struct LLVMPass : public PassInfoMixin<LLVMPass> {
   PreservedAnalyses run(Module &M, ModuleAnalysisManager &MAM);
 };
- PreservedAnalyses LLVMPass::run(Module &M, ModuleAnalysisManager &MAM) {
-   LLVMContext &Ctx = M.getContext();
-   IntegerType *Int32Ty = IntegerType::getInt32Ty(Ctx);
-   Type *Int8PtrTy = Type::getInt8PtrTy(Ctx);
 
-   FunctionCallee debug_func = M.getOrInsertFunction("debug", Int32Ty);
-   ConstantInt *debug_arg = ConstantInt::get(Int32Ty, 48763);
+PreservedAnalyses LLVMPass::run(Module &M, ModuleAnalysisManager &MAM) {
+  LLVMContext &Ctx = M.getContext();
+  IntegerType *Int32Ty = IntegerType::getInt32Ty(Ctx);
+  PointerType *CharPtrTy = Type::getInt8PtrTy(Ctx);
+  PointerType *CharPtrPtrTy = PointerType::getUnqual(CharPtrTy);
 
-   for (auto &F : M) {
-     errs() << "func: " << F.getName() << "\n";
-     if (F.getName() == "main") {
-       IRBuilder<> Builder(&*F.getEntryBlock().getFirstInsertionPt());
+  // Get reference to debug(int) function
+  FunctionCallee debug_func = M.getOrInsertFunction("debug", Type::getVoidTy(Ctx), Int32Ty);
+  ConstantInt *debug_arg = ConstantInt::get(Int32Ty, 48763);
 
-       Builder.CreateCall(debug_func, debug_arg);
+  for (auto &F : M) {
+    errs() << "func: " << F.getName() << "\n";
+    if (F.getName() == "main") {
+      IRBuilder<> Builder(&*F.getEntryBlock().getFirstInsertionPt());
 
-       Argument *ArgcArg = F.getArg(0);
-       ArgcArg->replaceAllUsesWith(debug_arg);
+      // Call debug(48763);
+      Builder.CreateCall(debug_func, {debug_arg});
 
-       Argument *ArgvArg = F.getArg(1);
-       Value *GEP = Builder.CreateInBoundsGEP(
-           Int8PtrTy,
-           ArgvArg,
-           ConstantInt::get(Int32Ty, 1)
-       );
-       Value *StrVal = Builder.CreateGlobalStringPtr("hayaku... motohayaku!");
-       Builder.CreateStore(StrVal, GEP);
-     }
-   }
-   return PreservedAnalyses::none();
- }
+      // argc = 48763
+      Argument *argcArg = F.getArg(0);
+      argcArg->replaceAllUsesWith(debug_arg);
+
+      // argv[1] = "hayaku... motohayaku!"
+      Argument *argvArg = F.getArg(1);
+      Value *index1 = ConstantInt::get(Int32Ty, 1);
+      Value *argv1_ptr = Builder.CreateInBoundsGEP(CharPtrTy, argvArg, index1);
+      Value *hayakuStr = Builder.CreateGlobalStringPtr("hayaku... motohayaku!");
+      Builder.CreateStore(hayakuStr, argv1_ptr);
+    }
+  }
+  return PreservedAnalyses::none();
+}
+
 extern "C" ::llvm::PassPluginLibraryInfo LLVM_ATTRIBUTE_WEAK
 llvmGetPassPluginInfo() {
   return {LLVM_PLUGIN_API_VERSION, "LLVMPass", "1.0",
     [](PassBuilder &PB) {
       PB.registerOptimizerLastEPCallback(
-
-
         [](ModulePassManager &MPM, OptimizationLevel OL) {
           MPM.addPass(LLVMPass());
         });
