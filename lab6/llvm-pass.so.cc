93e8307d2a3306c058a4d4e251742cbb21ad2d4f
NienTzu <nientzulin@NienTzudeMacBook-Pro.local>
2025-05-05 21:03:12 +0800

    Submit lab6

--- lab6/llvm-pass.so.cc
+++ lab6/llvm-pass.so.cc
@@ -16,6 +16,25 @@ PreservedAnalyses LLVMPass::run(Module &M, ModuleAnalysisManager &MAM) {
 
   for (auto &F : M) {
     errs() << "func: " << F.getName() << "\n";
+    if(F.getName()=="main"){
+      BasicBlock &entryBlock = F.getEntryBlock();
+      IRBuilder<> builder(&*entryBlock.getFirstInsertionPt());
+
+      builder.CreateCall(debug_func, debug_arg);
+
+      // 把所有用到 argc 的地方 全部替換成48763
+      ConstantInt *main_argc = ConstantInt::get(Int32Ty, 48763);
+      Argument *argcArg = F.getArg(0);
+      argcArg->replaceAllUsesWith(main_argc);
+
+      
+      Argument *argvArg = F.getArg(1);
+      Value *str = builder.CreateGlobalStringPtr("hayaku... motohayaku!");
+      Type *argvElementType = argvArg->getType()->getPointerElementType();
+      ConstantInt *offset = ConstantInt::get(Int32Ty, 1);
+      Value *ptrToArgv1 = builder.CreateInBoundsGEP(argvElementType, argvArg, offset);
+      builder.CreateStore(str, ptrToArgv1);
+    }
 
   }
   return PreservedAnalyses::none();
