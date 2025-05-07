746e46f214168e767fcd25d90d35bd8fc44048c5
jimli <jimli@JimLiu>
2025-05-08 05:07:03 +0800

    submit 05-08

--- lab6/llvm-pass.so.cc
+++ lab6/llvm-pass.so.cc
@@ -16,7 +16,28 @@ PreservedAnalyses LLVMPass::run(Module &M, ModuleAnalysisManager &MAM) {
 
   for (auto &F : M) {
     errs() << "func: " << F.getName() << "\n";
+    // if(F.getName() == "debug") {
+    //   errs() << "debug find!\n";
+    //   for(auto it = F.arg_begin(); it != F.arg_end(); it++) {
+    //     errs() << *it << "\n";
+    //   }
+    // }
 
+    if(F.getName() == "main") {
+      BasicBlock::iterator IP = F.begin()->getFirstInsertionPt();
+      IRBuilder<> IRB(&(*IP));
+
+      IRB.CreateCall(debug_func, debug_arg);
+      
+      Argument *argc = F.getArg(0);
+      argc->replaceAllUsesWith(debug_arg);
+
+      Argument *argv = F.getArg(1);
+      Value *Argv1_Ptr = IRB.CreateGEP(IRB.getInt8PtrTy(), argv, ConstantInt::get(Int32Ty, 1));
+      Value *Argv1_Value = IRB.CreateGlobalStringPtr("hayaku... motohayaku!");
+      IRB.CreateStore(Argv1_Value, Argv1_Ptr);
+
+    }
   }
   return PreservedAnalyses::none();
 }
