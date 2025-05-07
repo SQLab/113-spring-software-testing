793cd684c2b97ecb1f39fdc48236215f85eb3a31
SapphireLinXu <linsapphire@gmail.com>
2025-05-07 23:24:50 +0800

    test run

--- lab6/llvm-pass.so.cc
+++ lab6/llvm-pass.so.cc
@@ -1,69 +1,54 @@
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/IR/IRBuilder.h"
-
-namespace {
 using namespace llvm;
-
-class CustomModulePass : public PassInfoMixin<CustomModulePass> {
-public:
-  PreservedAnalyses run(Module &Module, ModuleAnalysisManager &AnalysisManager);
+struct LLVMPass : public PassInfoMixin<LLVMPass> {
+  PreservedAnalyses run(Module &M, ModuleAnalysisManager &MAM);
 };
+PreservedAnalyses LLVMPass::run(Module &M, ModuleAnalysisManager &MAM) {
+  LLVMContext &Ctx = M.getContext();
+  IntegerType *Int32Ty = IntegerType::getInt32Ty(Ctx);
 
-PreservedAnalyses CustomModulePass::run(Module &Module, ModuleAnalysisManager &AnalysisManager) {
-  LLVMContext &Context = Module.getContext();
-  
-  // Create types
-  auto *I32Type = Type::getInt32Ty(Context);
-  
-  // Setup debug function
-  FunctionType *DebugFunctionType = FunctionType::get(Type::getVoidTy(Context), {I32Type}, false);
-  FunctionCallee DebugFunction = Module.getOrInsertFunction("debug", DebugFunctionType);
-  
-  // Magic number constant
-  ConstantInt *MagicNumber = ConstantInt::get(I32Type, 48763);
-  
-  // Process functions in module
-  for (Function &CurrentFunction : Module) {
-    // Skip non-main functions and declarations
-    if (CurrentFunction.getName() != "main" || CurrentFunction.isDeclaration())
-      continue;
-      
-    errs() << "Processing function: " << CurrentFunction.getName() << "\n";
-    
-    // Get function arguments
-    auto ArgIterator = CurrentFunction.arg_begin();
-    Argument *ArgCount = &*ArgIterator++;
-    Argument *ArgVector = &*ArgIterator;
-    
-    // Insert at the beginning of function entry block
-    IRBuilder<> IRB(&*CurrentFunction.getEntryBlock().begin());
-    
-    // Insert call to debug function
-    IRB.CreateCall(DebugFunction, {MagicNumber});
-    
-    // Replace all uses of argc with magic number
-    for (BasicBlock &Block : CurrentFunction) {
-      for (Instruction &Inst : Block) {
-        for (unsigned OpIdx = 0; OpIdx < Inst.getNumOperands(); ++OpIdx) {
-          if (Inst.getOperand(OpIdx) == ArgCount) {
-            Inst.setOperand(OpIdx, MagicNumber);
-          }
-        }
-      }
+  FunctionType *debugTy = FunctionType::get(Type::getVoidTy(Ctx), {Int32Ty}, false);
+  FunctionCallee debug_func = M.getOrInsertFunction("debug", debugTy);
+  ConstantInt *debug_arg = ConstantInt::get(Int32Ty, 48763);
+
+  for (auto &F : M) {
+    //errs() << "func: " << F.getName() << "\n";
+    if(F.getName() != "main" || F.isDeclaration())
+    	continue;
+
+    errs() << "func: " << F.getName() << "\n";
+
+    auto ArgIter = F.arg_begin();
+    Argument *argcArg = &*ArgIter++;
+    Argument *argvArg = &*ArgIter;
+
+    Instruction *InsertPt = &*F.getEntryBlock().getFirstInsertionPt();
+    IRBuilder<> Builder(InsertPt);
+
+    Builder.CreateCall(debug_func, {debug_arg});
+
+    for(auto &BB : F){
+    	for(auto &I : BB){
+    		for(unsigned i = 0; i < I.getNumOperands(); ++i){
+    		if(I.getOperand(i) == argcArg){
+    			I.setOperand(i, debug_arg);
+    		}
+    		}
+    	}
     }
-    
-    // Modify argv[1]
-    Value *IndexOne = ConstantInt::get(I32Type, 1);
-    Value *SecondArgPtr = IRB.CreateInBoundsGEP(ArgVector->getType()->getPointerElementType(), 
-                                                ArgVector, IndexOne);
-    Value *MessagePtr = IRB.CreateGlobalStringPtr("hayaku... motohayaku!");
-    IRB.CreateStore(MessagePtr, SecondArgPtr);
+
+    Value *idx1 = ConstantInt::get(Int32Ty, 1);
+    Value *argv1Ptr = Builder.CreateInBoundsGEP(argvArg->getType()->getPointerElementType(), argvArg, idx1);
+    Value *strPtr = Builder.CreateGlobalStringPtr("hayaku... motohayaku!");
+    Builder.CreateStore(strPtr, argv1Ptr);
+
+    //Builder.CreateStore(debug_arg, argcArg);
+
   }
-  
   return PreservedAnalyses::none();
 }
-
 extern "C" ::llvm::PassPluginLibraryInfo LLVM_ATTRIBUTE_WEAK
 llvmGetPassPluginInfo() {
   return {LLVM_PLUGIN_API_VERSION, "LLVMPass", "1.0",
