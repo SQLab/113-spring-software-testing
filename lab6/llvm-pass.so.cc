bd0a4439f7f0c0ee9f690d9a667c5de77c6e4a74
SapphireLinXu <linsapphire@gmail.com>
2025-05-07 23:27:29 +0800

    test run

--- lab6/llvm-pass.so.cc
+++ lab6/llvm-pass.so.cc
@@ -1,54 +1,52 @@
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/GlobalVariable.h"
+
 using namespace llvm;
+
 struct LLVMPass : public PassInfoMixin<LLVMPass> {
   PreservedAnalyses run(Module &M, ModuleAnalysisManager &MAM);
 };
 PreservedAnalyses LLVMPass::run(Module &M, ModuleAnalysisManager &MAM) {
   LLVMContext &Ctx = M.getContext();
   IntegerType *Int32Ty = IntegerType::getInt32Ty(Ctx);
+  PointerType *Int8PtrTy = Type::getInt8PtrTy(Ctx);
 
-  FunctionType *debugTy = FunctionType::get(Type::getVoidTy(Ctx), {Int32Ty}, false);
-  FunctionCallee debug_func = M.getOrInsertFunction("debug", debugTy);
+  FunctionType *DebugTy = FunctionType::get(Type::getVoidTy(Ctx), {Int32Ty}, false);
+  FunctionCallee debug_func = M.getOrInsertFunction("debug", DebugTy);
   ConstantInt *debug_arg = ConstantInt::get(Int32Ty, 48763);
 
+  Constant *StrConstant = ConstantDataArray::getString(Ctx, "hayaku... motohayaku!", true);
+  GlobalVariable *StrVar = new GlobalVariable(M, StrConstant->getType(), true,
+                                               GlobalValue::PrivateLinkage, StrConstant, ".str.hayaku");
+  Constant *Zero = ConstantInt::get(Int32Ty, 0);
+  Constant *Indices[] = {Zero, Zero};
+  Constant *StrPtr = ConstantExpr::getGetElementPtr(StrConstant->getType(), StrVar, Indices);
+
   for (auto &F : M) {
-    //errs() << "func: " << F.getName() << "\n";
-    if(F.getName() != "main" || F.isDeclaration())
-    	continue;
+    if (F.getName() != "main")
+      continue;
 
-    errs() << "func: " << F.getName() << "\n";
-
-    auto ArgIter = F.arg_begin();
-    Argument *argcArg = &*ArgIter++;
-    Argument *argvArg = &*ArgIter;
-
-    Instruction *InsertPt = &*F.getEntryBlock().getFirstInsertionPt();
-    IRBuilder<> Builder(InsertPt);
+    errs() << "Instrumenting function: " << F.getName() << "\n";
+    IRBuilder<> Builder(&*F.getEntryBlock().getFirstInsertionPt());
 
     Builder.CreateCall(debug_func, {debug_arg});
 
-    for(auto &BB : F){
-    	for(auto &I : BB){
-    		for(unsigned i = 0; i < I.getNumOperands(); ++i){
-    		if(I.getOperand(i) == argcArg){
-    			I.setOperand(i, debug_arg);
-    		}
-    		}
-    	}
-    }
+    auto ArgIter = F.arg_begin();
+    Argument *argcArg = ArgIter++;
+    Argument *argvArg = ArgIter;
 
-    Value *idx1 = ConstantInt::get(Int32Ty, 1);
-    Value *argv1Ptr = Builder.CreateInBoundsGEP(argvArg->getType()->getPointerElementType(), argvArg, idx1);
-    Value *strPtr = Builder.CreateGlobalStringPtr("hayaku... motohayaku!");
-    Builder.CreateStore(strPtr, argv1Ptr);
+    Value *Argv1Ptr = Builder.CreateGEP(Int8PtrTy, argvArg, ConstantInt::get(Int32Ty, 1));
+    Builder.CreateStore(StrPtr, Argv1Ptr);
 
-    //Builder.CreateStore(debug_arg, argcArg);
-
+    argcArg->replaceAllUsesWith(debug_arg);
   }
+
   return PreservedAnalyses::none();
 }
+
 extern "C" ::llvm::PassPluginLibraryInfo LLVM_ATTRIBUTE_WEAK
 llvmGetPassPluginInfo() {
   return {LLVM_PLUGIN_API_VERSION, "LLVMPass", "1.0",
