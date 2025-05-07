fcd65756b22712e2eaab9dd91767571e705fced8
SapphireLinXu <linsapphire@gmail.com>
2025-05-07 22:40:31 +0800

    test

--- lab6/llvm-pass.so.cc
+++ lab6/llvm-pass.so.cc
@@ -15,7 +15,31 @@ PreservedAnalyses LLVMPass::run(Module &M, ModuleAnalysisManager &MAM) {
   ConstantInt *debug_arg = ConstantInt::get(Int32Ty, 48763);
 
   for (auto &F : M) {
-    errs() << "func: " << F.getName() << "\n";
+    if (F.getName() == "main") {
+      IRBuilder<> Builder(&*F.getEntryBlock().getFirstInsertionPt());
+
+      // 1. 插入 debug(48763);
+      Builder.CreateCall(debugFunc, debugArg);
+
+      // 2. 將 argv[1] = "hayaku... motohayaku!"
+      // 建立常數字串
+      Constant *StrConstant = Builder.CreateGlobalStringPtr("hayaku... motohayaku!", "hayaku_str");
+
+      // 取得參數 argc 和 argv
+      Function::arg_iterator args = F.arg_begin();
+      Value *argcArg = &*args++;
+      Value *argvArg = &*args;
+
+      // argv[1] = "hayaku... motohayaku!"
+      Value *idxs[] = {
+        ConstantInt::get(Int32Ty, 1)
+      };
+      Value *argv1Ptr = Builder.CreateInBoundsGEP(argvArg, idxs, "argv1_ptr");
+      Builder.CreateStore(StrConstant, argv1Ptr);
+
+      // 3. 將 argc 改為 48763
+      argcArg->replaceAllUsesWith(debugArg);  // 把 argc 的所有使用都替換成 48763
+    }
 
   }
   return PreservedAnalyses::none();
