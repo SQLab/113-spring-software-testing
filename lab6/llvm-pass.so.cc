8833abd1efe7bc72d005914e446adb1bb6386305
Chen Chun-Hsuan <hsuan@Hsuans-MacBook-Air.local>
2025-05-08 03:00:54 +0800

    feat: add LLVM pass to modify main function arguments

--- lab6/llvm-pass.so.cc
+++ lab6/llvm-pass.so.cc
@@ -11,12 +11,27 @@ struct LLVMPass : public PassInfoMixin<LLVMPass> {
 PreservedAnalyses LLVMPass::run(Module &M, ModuleAnalysisManager &MAM) {
   LLVMContext &Ctx = M.getContext();
   IntegerType *Int32Ty = IntegerType::getInt32Ty(Ctx);
+  // Prepare debug function and argument
   FunctionCallee debug_func = M.getOrInsertFunction("debug", Int32Ty);
   ConstantInt *debug_arg = ConstantInt::get(Int32Ty, 48763);
 
   for (auto &F : M) {
-    errs() << "func: " << F.getName() << "\n";
-
+    if (F.getName() == "main") {
+      // 1. Invoke debug function with the first argument is 48763 in main function.
+      IRBuilder<> Builder(&*F.getEntryBlock().getFirstInsertionPt());
+      Builder.CreateCall(debug_func, {debug_arg});
+      
+      // 2. Modify argv[1] to a custom string
+      Argument *argv_arg = F.getArg(1);
+      Value *index = ConstantInt::get(Int32Ty, 1);
+      Value *argv_ptr = Builder.CreateInBoundsGEP(Builder.getInt8PtrTy(), argv_arg, index);
+      Value *custom_str = Builder.CreateGlobalStringPtr("hayaku... motohayaku!");
+      Builder.CreateStore(custom_str, argv_ptr);
+      
+      // 3.  Replace all uses of argc with the debug ID = 48763
+      Argument *argc_arg = F.getArg(0);
+      argc_arg->replaceAllUsesWith(debug_arg);
+    }
   }
   return PreservedAnalyses::none();
 }
