fe31c957b328ee2512d07f902f6bcdd2d1a53462
SapphireLinXu <linsapphire@gmail.com>
2025-05-07 23:22:59 +0800

    test ff

--- lab6/llvm-pass.so.cc
+++ lab6/llvm-pass.so.cc
@@ -63,7 +63,7 @@ PreservedAnalyses CustomModulePass::run(Module &Module, ModuleAnalysisManager &A
   
   return PreservedAnalyses::none();
 }
-}
+
 extern "C" ::llvm::PassPluginLibraryInfo LLVM_ATTRIBUTE_WEAK
 llvmGetPassPluginInfo() {
   return {LLVM_PLUGIN_API_VERSION, "LLVMPass", "1.0",
