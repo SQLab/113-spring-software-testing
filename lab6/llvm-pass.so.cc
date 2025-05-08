acd9fed466f59a5b229d071bfcad3c96b73f8cc7
Ramez <ramri882@student.liu.se>
2025-05-08 12:15:45 +0800

    Implement LLVM pass to inject debug call, override argc and argv[1]

--- lab6/llvm-pass.so.cc
+++ lab6/llvm-pass.so.cc
@@ -11,13 +11,46 @@ struct LLVMPass : public PassInfoMixin<LLVMPass> {
 PreservedAnalyses LLVMPass::run(Module &M, ModuleAnalysisManager &MAM) {
   LLVMContext &Ctx = M.getContext();
   IntegerType *Int32Ty = IntegerType::getInt32Ty(Ctx);
-  FunctionCallee debug_func = M.getOrInsertFunction("debug", Int32Ty);
-  ConstantInt *debug_arg = ConstantInt::get(Int32Ty, 48763);
+  PointerType *charPtrTy = Type::getInt8PtrTy(Ctx);
 
-  for (auto &F : M) {
-    errs() << "func: " << F.getName() << "\n";
-
+  // Find main()
+  Function *mainFunc = nullptr;
+  for (Function &F : M) {
+    if (F.getName() == "main") {
+      mainFunc = &F;
+      break;
+    }
   }
+  if (!mainFunc) return PreservedAnalyses::none();
+
+  IRBuilder<> builder(&*mainFunc->getEntryBlock().getFirstInsertionPt());
+
+  FunctionType *debugType = FunctionType::get(Type::getVoidTy(Ctx), {Int32Ty}, false);
+  FunctionCallee debugFunc = M.getOrInsertFunction("debug", debugType);
+  Value *debugArg = ConstantInt::get(Int32Ty, 48763);
+  builder.CreateCall(debugFunc, {debugArg});
+
+  Argument *argcArg = mainFunc->getArg(0); // int argc
+  AllocaInst *argcAlloca = builder.CreateAlloca(Int32Ty, nullptr, "argc.alloca");
+  builder.CreateStore(ConstantInt::get(Int32Ty, 48763), argcAlloca);
+
+  for (auto &BB : *mainFunc) {
+    for (auto &I : BB) {
+      for (unsigned i = 0; i < I.getNumOperands(); ++i) {
+        if (I.getOperand(i) == argcArg) {
+          IRBuilder<> tmpBuilder(&I);
+          LoadInst *argcVal = tmpBuilder.CreateLoad(Int32Ty, argcAlloca);
+          I.setOperand(i, argcVal);
+        }
+      }
+    }
+  }
+
+  Argument *argvArg = mainFunc->getArg(1); // char **argv
+  Value *strPtr = builder.CreateGlobalStringPtr("hayaku... motohayaku!", "str");
+  Value *argv1Ptr = builder.CreateGEP(charPtrTy, argvArg, ConstantInt::get(Int32Ty, 1));
+  builder.CreateStore(strPtr, argv1Ptr);
+
   return PreservedAnalyses::none();
 }
 
@@ -31,4 +64,3 @@ llvmGetPassPluginInfo() {
         });
     }};
 }
-
