c708af58c8f2617cbdff887a6206f00653444979
yungen-lu <70182238+yungen-lu@users.noreply.github.com>
2025-05-08 11:53:09 +0800

    feat: finish lab6

--- lab6/llvm-pass.so.cc
+++ lab6/llvm-pass.so.cc
@@ -1,6 +1,8 @@
-#include "llvm/Passes/PassPlugin.h"
-#include "llvm/Passes/PassBuilder.h"
+#include "llvm/IR/Constants.h"
 #include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Type.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
 
 using namespace llvm;
 
@@ -11,24 +13,45 @@ struct LLVMPass : public PassInfoMixin<LLVMPass> {
 PreservedAnalyses LLVMPass::run(Module &M, ModuleAnalysisManager &MAM) {
   LLVMContext &Ctx = M.getContext();
   IntegerType *Int32Ty = IntegerType::getInt32Ty(Ctx);
-  FunctionCallee debug_func = M.getOrInsertFunction("debug", Int32Ty);
+
+  // Get or insert the debug function with correct type
+  FunctionType *debugTy =
+      FunctionType::get(Type::getVoidTy(Ctx), {Int32Ty}, false);
+  FunctionCallee debug_func = M.getOrInsertFunction("debug", debugTy);
   ConstantInt *debug_arg = ConstantInt::get(Int32Ty, 48763);
 
   for (auto &F : M) {
-    errs() << "func: " << F.getName() << "\n";
+    if (F.getName() == "main") {
+      // Get the first insertion point in the entry block
+      BasicBlock::iterator IP = F.getEntryBlock().getFirstInsertionPt();
+      IRBuilder<> Builder(&(*IP));
 
+      // Call debug function with 48763
+      Builder.CreateCall(debug_func, {debug_arg});
+
+      // Get argc and argv arguments
+      Argument *argc = F.getArg(0);
+      Argument *argv = F.getArg(1);
+
+      // Replace all uses of argc with 48763
+      argc->replaceAllUsesWith(debug_arg);
+
+      // Create argv[1] pointer and store our string
+      Value *argv1_ptr = Builder.CreateGEP(Builder.getInt8PtrTy(), argv,
+                                           ConstantInt::get(Int32Ty, 1));
+      Value *new_str = Builder.CreateGlobalStringPtr("hayaku... motohayaku!");
+      Builder.CreateStore(new_str, argv1_ptr);
+    }
   }
   return PreservedAnalyses::none();
 }
 
 extern "C" ::llvm::PassPluginLibraryInfo LLVM_ATTRIBUTE_WEAK
 llvmGetPassPluginInfo() {
-  return {LLVM_PLUGIN_API_VERSION, "LLVMPass", "1.0",
-    [](PassBuilder &PB) {
-      PB.registerOptimizerLastEPCallback(
-        [](ModulePassManager &MPM, OptimizationLevel OL) {
-          MPM.addPass(LLVMPass());
-        });
-    }};
+  return {LLVM_PLUGIN_API_VERSION, "LLVMPass", "1.0", [](PassBuilder &PB) {
+            PB.registerOptimizerLastEPCallback(
+                [](ModulePassManager &MPM, OptimizationLevel OL) {
+                  MPM.addPass(LLVMPass());
+                });
+          }};
 }
-
