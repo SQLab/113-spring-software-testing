05abaa66872f5e70b2c414505c796992878ec362
YingMuo <wl03452329@gmail.com>
2025-05-15 12:29:32 +0800

    feat: lab8

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,18 +1,6 @@
-#include <stdint.h>
+#include <string.h>
 
 void antiasan(unsigned long addr)
 {
-    // 1. 計算 gBadBuf 對應的 shadow memory 起點 (8:1 映射)
-    unsigned char *shadow_ptr = (unsigned char *)(((uintptr_t)addr >> 3) + 0x7FFF8000);
-    // 2. 往回掃過 gBadBuf 的 0x00 區段 (合法 shadow bytes)
-    while (*shadow_ptr == 0x00) {
-        ++shadow_ptr;
-    }
-    // 3. 再往前掃過整段 red-zone (0xF0~0xFF)
-    while (*shadow_ptr > 0x00) {
-        ++shadow_ptr;
-    }
-    for(int i=0; i<8; i++){
-        shadow_ptr[i] = 0x00;
-    }
+
 }
