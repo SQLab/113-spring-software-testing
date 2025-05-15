42b748adcd4a4cd534856fa01287de1877b62da1
YingMuo <wl03452329@gmail.com>
2025-05-15 12:29:31 +0800

    feat: lab8

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,24 +1,6 @@
-#include "antiasan.h"
-#include <stdint.h>
-
-#ifdef __APPLE__
+#include <string.h>
 
 void antiasan(unsigned long addr)
 {
-    (void)addr;
+
 }
-
-#else
-
-void antiasan(unsigned long addr)
-{
-    const unsigned long SHADOW_OFFSET = 0x7fff8000;
-
-    unsigned long start_addr = (addr - 0x100);
-
-    unsigned char* shadow = (unsigned char *)((start_addr >> 3) + SHADOW_OFFSET);
-    for (int i = 0; i < 64; i++) {
-        shadow[i] = 0;
-    }
-}
-#endif
