cdae9d4e8afdc0f5f19df9ed73a1fbab690dbb37
YingMuo <wl03452329@gmail.com>
2025-05-15 12:29:31 +0800

    feat: lab8

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,13 +1,6 @@
 #include <string.h>
 
-void antiasan(unsigned long addr) {
-  unsigned long start = addr + 0x87;
-  unsigned long end = start + 0x87;
+void antiasan(unsigned long addr)
+{
 
-  for (unsigned long p = start; p < end; p += 8) {
-    unsigned char *shadow = (unsigned char *)((p >> 3) + 0x7fff8000);
-    strcpy((char *)shadow, "\0");
-    // Or just set the shadow to 0
-    // *shadow = 0;
-  }
 }
