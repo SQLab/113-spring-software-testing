973359e99e2c03cd079f32eaf2479458e72dbbfd
YingMuo <wl03452329@gmail.com>
2025-05-15 12:29:32 +0800

    feat: lab8

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,11 +1,6 @@
-#include <stdint.h>
+#include <string.h>
 
-void antiasan(unsigned long addr) {
-    unsigned long gBadBuf = addr;
-    unsigned long gS = gBadBuf + 0xC0; 
-    unsigned long shadow_base = 0x7fff8000;  // ASAN shadow memory offset
-    unsigned char *shadow_gS_end = (unsigned char *)((gS + sizeof(char) * 24) >> 3) + shadow_base; 
-    for (int i = 0; i < 5; i++) {
-        shadow_gS_end[i] = 0;
-    }
+void antiasan(unsigned long addr)
+{
+
 }
