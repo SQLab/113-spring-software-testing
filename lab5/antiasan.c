4eaa2443308b888be819615c2c4dc1e588f43b72
YingMuo <wl03452329@gmail.com>
2025-05-15 12:29:31 +0800

    feat: lab8

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,9 +1,6 @@
-#include <sanitizer/asan_interface.h>
+#include <string.h>
 
-//TODO:
 void antiasan(unsigned long addr)
 {
-    // __asan_unpoison_memory_region((void *)addr, 64);
-    // __asan_unpoison_memory_region((void *)addr, 128);
-    __asan_unpoison_memory_region((void *)addr, 256);
+
 }
