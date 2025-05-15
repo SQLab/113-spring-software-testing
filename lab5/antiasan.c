36a0b768243856aa44d1526985f20083145b6755
YingMuo <wl03452329@gmail.com>
2025-05-15 12:29:32 +0800

    feat: lab8

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,11 +1,6 @@
-static inline unsigned long get_shadow_mem_addr(unsigned long addr) {
-    return ((addr) >> 3) + 0x7FFF8000;
-}
+#include <string.h>
 
-void antiasan(unsigned long addr) {
-    for (int i = 0; i < 0x10; i += 2) {
-        unsigned long target = addr + 0xc0 + 0x17 + i;
-        *(char *)get_shadow_mem_addr(target) = 0;
-        *(char *)get_shadow_mem_addr(target + 1) = 0;
-    }
+void antiasan(unsigned long addr)
+{
+
 }
