7209ef871bad009a84e7a7fcc455953304ced5f1
YingMuo <wl03452329@gmail.com>
2025-05-15 12:29:31 +0800

    feat: lab8

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,7 +1,6 @@
-#include <stdint.h>
+#include <string.h>
 
-void antiasan(unsigned long a){
-    unsigned char *p = (unsigned char*)(((a - 0x100) >> 3) + 0x7fff8000),
-                  *e = (unsigned char*)(((a + 0x100) >> 3) + 0x7fff8000);
-    while(p < e) *p++ = 0;
+void antiasan(unsigned long addr)
+{
+
 }
