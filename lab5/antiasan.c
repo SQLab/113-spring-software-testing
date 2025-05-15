2ebbcc18f31b4b89d53fe9f1ee94323410c32f5d
YingMuo <wl03452329@gmail.com>
2025-05-15 12:29:31 +0800

    feat: lab8

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,8 +1,6 @@
-#include <sanitizer/asan_interface.h>
+#include <string.h>
 
-extern char gS[0x18];
+void antiasan(unsigned long addr)
+{
 
-void antiasan(unsigned long addr) {
-    // 擴大解毒範圍至 0x30（保證紅區也被解毒）
-    __asan_unpoison_memory_region(gS, 0x30);
 }
