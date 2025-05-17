be02417c7fd6701e540e22bbaa48ae9091b635d2
sa-llo <samuel.lloyd989@gmail.com>
2025-05-17 17:33:31 +0800

    tried solving it, still failed using validate script

--- lab8/solve.py
+++ lab8/solve.py
@@ -8,27 +8,25 @@ def main():
     project = angr.Project('./chal', auto_load_libs=False)
 
     input_len = 8
-    chars = [claripy.BVS('', 8) for _ in range(input_len)]
-    buf = claripy.Concat(*chars, claripy.BVV(0, 8))  # Add null terminator!
+    input_chars = [claripy.BVS('', 8) for _ in range(input_len)]
+    sym_input = claripy.Concat(*input_chars)
+    full_input = claripy.Concat(sym_input, claripy.BVV(0, 8))
 
-    state = project.factory.entry_state(stdin=buf)
+    state = project.factory.entry_state(stdin=full_input)
 
-    for c in chars:
+    for c in input_chars:
         state.solver.add(c >= 0x20)
         state.solver.add(c <= 0x7e)
 
     simgr = project.factory.simgr(state)
-
     simgr.explore(
-        find=lambda s: b"Correct!" in s.posix.dumps(1),
+        find=lambda s: b"CTF{" in s.posix.dumps(1),
         avoid=lambda s: b"Wrong key" in s.posix.dumps(1)
     )
 
     if simgr.found:
-        sol = simgr.found[0].solver.eval(buf, cast_to=bytes)
-        print(sol.decode(), end='')  # Print cleanly
-    else:
-        print("[-] No solution found.", end='')
+        solution = simgr.found[0].solver.eval(sym_input, cast_to=bytes)
+        print(solution.decode(), end='')
 
 if __name__ == '__main__':
     main()
