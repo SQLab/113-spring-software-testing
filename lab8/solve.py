1ec0b20fed46fa88674c14f2073825a1a594fe9e
蕭皓隆 <hsiao618@xiaohaolongdeMacBook-Pro.local>
2025-05-18 17:52:30 +0800

    finish lab8

--- lab8/solve.py
+++ lab8/solve.py
@@ -1,11 +1,45 @@
 #!/usr/bin/env python3
 
-import angr,sys
+import angr
+import claripy
+import sys
 
 def main():
-    secret_key = b""
-    sys.stdout.buffer.write(secret_key)
-
+    # 創建 angr 專案
+    proj = angr.Project('./chal', auto_load_libs=False)
+    
+    # 創建符號輸入
+    input_size = 8
+    # 創建 8 個符號字元
+    sym_chars = [claripy.BVS(f'char_{i}', 8) for i in range(input_size)]
+    # 將字元連接成一個字串
+    sym_input = claripy.Concat(*sym_chars)
+    
+    # 創建初始狀態
+    state = proj.factory.entry_state(stdin=sym_input)
+    
+    # 添加約束條件
+    for char in sym_chars:
+        state.solver.add(char >= 0x20)  # 可列印字元
+        state.solver.add(char <= 0x7e)
+    
+    # 創建模擬管理器
+    simgr = proj.factory.simulation_manager(state)
+    
+    # 探索直到找到目標
+    simgr.explore(
+        find=lambda s: b"Correct!" in s.posix.dumps(1),
+        avoid=lambda s: b"Wrong key!" in s.posix.dumps(1)
+    )
+    
+    if len(simgr.found) > 0:
+        # 獲取找到的解決方案
+        solution = simgr.found[0].solver.eval(sym_input, cast_to=bytes)
+        # 輸出密鑰
+        sys.stdout.buffer.write(solution)
+    else:
+        print("No solution found", file=sys.stderr)
+        sys.exit(1)
 
 if __name__ == '__main__':
     main()
