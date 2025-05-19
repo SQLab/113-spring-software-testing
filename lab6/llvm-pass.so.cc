f5dcd1e0efbb935ffcd93ccf9e01836c0e3e71aa
YingMuo <wl03452329@gmail.com>
2025-05-19 18:54:47 +0800

    fix: lab8 no angr

--- lab6/llvm-pass.so.cc
+++ lab6/llvm-pass.so.cc
@@ -10,34 +10,13 @@ struct LLVMPass : public PassInfoMixin<LLVMPass> {
 
 PreservedAnalyses LLVMPass::run(Module &M, ModuleAnalysisManager &MAM) {
   LLVMContext &Ctx = M.getContext();
-  IRBuilder<> Builder(Ctx);
-
   IntegerType *Int32Ty = IntegerType::getInt32Ty(Ctx);
-  PointerType *I8PtrTy  = Type::getInt8PtrTy(Ctx); // for argv
-
   FunctionCallee debug_func = M.getOrInsertFunction("debug", Int32Ty);
   ConstantInt *debug_arg = ConstantInt::get(Int32Ty, 48763);
-  Constant *StrGlobal = Builder.CreateGlobalStringPtr("hayaku... motohayaku!", "",
-    0, &M); // for argv
 
   for (auto &F : M) {
-    if (F.getName() == "main"){
-      // insert debug
-      BasicBlock &Entry = F.getEntryBlock();
-      Builder.SetInsertPoint(&*Entry.getFirstInsertionPt());
-      Builder.CreateCall(debug_func, {debug_arg});
+    errs() << "func: " << F.getName() << "\n";
 
-      // insert argc, argv
-      auto ArgIter = F.arg_begin();
-      Argument *Argc = &*ArgIter++;
-      Argument *Argv = &*ArgIter; 
-      // replace argc -> i8, hayaku... motohayaku!
-      Value *Idx1    = Builder.getInt32(1);
-      Value *Argv1Ptr= Builder.CreateGEP(I8PtrTy, Argv, Idx1);
-      Builder.CreateStore(StrGlobal, Argv1Ptr);
-      // replace argv -> debug_arg 48763
-      if (!Argc->use_empty()) Argc->replaceAllUsesWith(debug_arg);
-    }
   }
   return PreservedAnalyses::none();
 }
