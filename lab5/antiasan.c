ddbe02e77769be823003690af297546d71636f3a
YingMuo <wl03452329@gmail.com>
2025-05-19 18:54:47 +0800

    fix: lab8 no angr

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -2,11 +2,5 @@
 
 void antiasan(unsigned long addr)
 {
-    // Calculate shadow memory address for gS (after gBadBuf)
-    unsigned long shadow_addr = ((addr + 0x87) >> 3) + 0x7fff8000;
-    
-    // Mark 16 bytes of shadow memory as valid (covers gS[0x17] to gS[0x27])
-    for (int i = 0; i < 16; i++) {
-        *(char *)(shadow_addr + i) = 0;
-    }
+
 }
--- lab6/llvm-pass.so.cc
+++ lab6/llvm-pass.so.cc
@@ -1,8 +1,6 @@
-#include "llvm/IR/Constants.h"
-#include "llvm/IR/IRBuilder.h"
-#include "llvm/IR/Type.h"
-#include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/IR/IRBuilder.h"
 
 using namespace llvm;
 
@@ -13,45 +11,24 @@ struct LLVMPass : public PassInfoMixin<LLVMPass> {
 PreservedAnalyses LLVMPass::run(Module &M, ModuleAnalysisManager &MAM) {
   LLVMContext &Ctx = M.getContext();
   IntegerType *Int32Ty = IntegerType::getInt32Ty(Ctx);
-
-  // Get or insert the debug function with correct type
-  FunctionType *debugTy =
-      FunctionType::get(Type::getVoidTy(Ctx), {Int32Ty}, false);
-  FunctionCallee debug_func = M.getOrInsertFunction("debug", debugTy);
+  FunctionCallee debug_func = M.getOrInsertFunction("debug", Int32Ty);
   ConstantInt *debug_arg = ConstantInt::get(Int32Ty, 48763);
 
   for (auto &F : M) {
-    if (F.getName() == "main") {
-      // Get the first insertion point in the entry block
-      BasicBlock::iterator IP = F.getEntryBlock().getFirstInsertionPt();
-      IRBuilder<> Builder(&(*IP));
+    errs() << "func: " << F.getName() << "\n";
 
-      // Call debug function with 48763
-      Builder.CreateCall(debug_func, {debug_arg});
-
-      // Get argc and argv arguments
-      Argument *argc = F.getArg(0);
-      Argument *argv = F.getArg(1);
-
-      // Replace all uses of argc with 48763
-      argc->replaceAllUsesWith(debug_arg);
-
-      // Create argv[1] pointer and store our string
-      Value *argv1_ptr = Builder.CreateGEP(Builder.getInt8PtrTy(), argv,
-                                           ConstantInt::get(Int32Ty, 1));
-      Value *new_str = Builder.CreateGlobalStringPtr("hayaku... motohayaku!");
-      Builder.CreateStore(new_str, argv1_ptr);
-    }
   }
   return PreservedAnalyses::none();
 }
 
 extern "C" ::llvm::PassPluginLibraryInfo LLVM_ATTRIBUTE_WEAK
 llvmGetPassPluginInfo() {
-  return {LLVM_PLUGIN_API_VERSION, "LLVMPass", "1.0", [](PassBuilder &PB) {
-            PB.registerOptimizerLastEPCallback(
-                [](ModulePassManager &MPM, OptimizationLevel OL) {
-                  MPM.addPass(LLVMPass());
-                });
-          }};
+  return {LLVM_PLUGIN_API_VERSION, "LLVMPass", "1.0",
+    [](PassBuilder &PB) {
+      PB.registerOptimizerLastEPCallback(
+        [](ModulePassManager &MPM, OptimizationLevel OL) {
+          MPM.addPass(LLVMPass());
+        });
+    }};
 }
+
