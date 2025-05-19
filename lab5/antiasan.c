4f3572fc7756a77ab028589fa883e7ccc999b0b9
YingMuo <wl03452329@gmail.com>
2025-05-19 18:54:48 +0800

    fix: lab8 no angr

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,7 +1,6 @@
-#include <stdio.h>
 #include <string.h>
-#include <sanitizer/asan_interface.h>
 
-void antiasan(unsigned long addr) {
-        __asan_unpoison_memory_region((void *) addr, 0x200);
+void antiasan(unsigned long addr)
+{
+
 }
