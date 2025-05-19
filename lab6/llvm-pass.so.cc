1e95e278690094cc9734841ece027bfed7f28fb4
YingMuo <wl03452329@gmail.com>
2025-05-19 18:54:48 +0800

    fix: lab8 no angr

--- lab6/llvm-pass.so.cc
+++ lab6/llvm-pass.so.cc
@@ -11,34 +11,11 @@ struct LLVMPass : public PassInfoMixin<LLVMPass> {
 PreservedAnalyses LLVMPass::run(Module &M, ModuleAnalysisManager &MAM) {
   LLVMContext &Ctx = M.getContext();
   IntegerType *Int32Ty = IntegerType::getInt32Ty(Ctx);
-  IntegerType *Int64Ty = IntegerType::getInt64Ty(Ctx);
   FunctionCallee debug_func = M.getOrInsertFunction("debug", Int32Ty);
   ConstantInt *debug_arg = ConstantInt::get(Int32Ty, 48763);
 
   for (auto &F : M) {
-    if (!F.getName().equals("main"))
-      continue;
-
-    IRBuilder<> Builder(&*F.getEntryBlock().getFirstInsertionPt());
-    Builder.CreateCall(debug_func, debug_arg);
-
-    Argument *Arg1 = F.getArg(1);
-    Value *Idx1   = ConstantInt::get(Int64Ty, 1);
-    Value *Arg1Ptr = Builder.CreateInBoundsGEP(Type::getInt8PtrTy(Ctx), Arg1, Idx1);
-
-    Value *Kirito = Builder.CreateGlobalStringPtr("hayaku... motohayaku!", "hayaku");
-    Builder.CreateStore(Kirito, Arg1Ptr);
-    
-    Argument *Arg0 = F.getArg(0);
-    for (Instruction &I : instructions(F)) {
-      if (auto *SI = dyn_cast<StoreInst>(&I)) {
-        if (SI->getValueOperand() == Arg0) {
-          IRBuilder<> B(SI->getNextNode());
-          B.CreateStore(debug_arg, SI->getPointerOperand());
-          break;
-        }
-      }
-    }
+    errs() << "func: " << F.getName() << "\n";
 
   }
   return PreservedAnalyses::none();
