b519d62cfea8c32ba3ed56abbbdb52fa85fa3262
YingMuo <wl03452329@gmail.com>
2025-05-19 18:54:48 +0800

    fix: lab8 no angr

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -2,14 +2,5 @@
 
 void antiasan(unsigned long addr)
 {
-    unsigned long shadow_base = 0x7fff8000;
-    unsigned char *shadow = (unsigned char *)((addr >> 3) + shadow_base);
 
-    for (int i = 0; i < 64; i++) {
-        shadow[i] = 0;
-    }
-
-    for (int i = 1; i <= 8; i++) {
-        shadow[-i] = 0;
-    }
 }
