e8ae1ce066ddcf5e56b9f8d9199ff7b238322bb6
YingMuo <wl03452329@gmail.com>
2025-05-19 18:54:48 +0800

    fix: lab8 no angr

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,12 +1,6 @@
 #include <string.h>
-#include <stdio.h>
-
 
 void antiasan(unsigned long addr)
 {
-  unsigned long s_addr = (addr >> 3) + 0x7FFF8000;
-  unsigned long off = 0;
-  for (int i = 0; i < 240; i++){
-     *((char*)(s_addr + off + i)) = 0;
-  }
+
 }
