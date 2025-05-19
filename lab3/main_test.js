2949af2c38d4d8509c062f52c93203db4164c498
YingMuo <wl03452329@gmail.com>
2025-05-19 18:54:47 +0800

    fix: lab8 no angr

--- lab3/main_test.js
+++ lab3/main_test.js
@@ -1,26 +1,5 @@
-const { describe, it } = require('node:test');
+const {describe, it} = require('node:test');
 const assert = require('assert');
 const { Calculator } = require('./main');
 
-describe('Calculator', () => {
-    const calculator = new Calculator();
-
-    describe('exp()', () => {
-        it('should return correct exponentiation results', () => {
-            assert.strictEqual(calculator.exp(0), 1);
-            assert.strictEqual(calculator.exp(1), Math.exp(1));
-            assert.strictEqual(calculator.exp(-1), Math.exp(-1));
-            assert.strictEqual(calculator.exp(5), Math.exp(5));
-        });
-
-        it('should throw an error for non-finite numbers', () => {
-            assert.throws(() => calculator.exp(Infinity), /unsupported operand type/);
-            assert.throws(() => calculator.exp(-Infinity), /unsupported operand type/);
-            assert.throws(() => calculator.exp(NaN), /unsupported operand type/);
-        });
-
-        it('should throw an overflow error for large numbers', () => {
-            assert.throws(() => calculator.exp(1000), /overflow/);
-        });
-    });
-});
+// TODO: write your tests here
--- lab6/llvm-pass.so.cc
+++ lab6/llvm-pass.so.cc
@@ -1,7 +1,6 @@
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/IR/IRBuilder.h"
-#include "llvm/IR/Constants.h"
 
 using namespace llvm;
 
@@ -12,39 +11,13 @@ struct LLVMPass : public PassInfoMixin<LLVMPass> {
 PreservedAnalyses LLVMPass::run(Module &M, ModuleAnalysisManager &MAM) {
   LLVMContext &Ctx = M.getContext();
   IntegerType *Int32Ty = IntegerType::getInt32Ty(Ctx);
-  PointerType *CharPtrTy = Type::getInt8PtrTy(Ctx);
-  
-  // Get or create debug function declaration
-  FunctionCallee debug_func = M.getOrInsertFunction("debug", 
-    FunctionType::get(Type::getVoidTy(Ctx), {Int32Ty}, false));
-  
-  // Create constant value for debug function argument and argc
+  FunctionCallee debug_func = M.getOrInsertFunction("debug", Int32Ty);
   ConstantInt *debug_arg = ConstantInt::get(Int32Ty, 48763);
-  
+
   for (auto &F : M) {
-    // Find main function
-    if (F.getName() == "main") {
-      // Create IRBuilder at the beginning of the entry block
-      IRBuilder<> Builder(&*F.getEntryBlock().getFirstInsertionPt());
-      
-      // a. Call debug function with argument 48763
-      Builder.CreateCall(debug_func, {debug_arg});
-      
-      // Get function arguments
-      Argument *argcArg = F.getArg(0);
-      Argument *argvArg = F.getArg(1);
-      
-      // b. Set argv[1] = "hayaku... motohayaku!"
-      Value *index1 = ConstantInt::get(Int32Ty, 1);
-      Value *argv1_ptr = Builder.CreateInBoundsGEP(CharPtrTy, argvArg, index1);
-      Value *hayakuStr = Builder.CreateGlobalStringPtr("hayaku... motohayaku!");
-      Builder.CreateStore(hayakuStr, argv1_ptr);
-      
-      // c. Replace all uses of argc with 48763
-      argcArg->replaceAllUsesWith(debug_arg);
-    }
+    errs() << "func: " << F.getName() << "\n";
+
   }
-  
   return PreservedAnalyses::none();
 }
 
@@ -58,3 +31,4 @@ llvmGetPassPluginInfo() {
         });
     }};
 }
+
