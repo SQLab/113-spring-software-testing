fa49d155259bd1138d22b7f7066c5fc19d4261fc
skysoul1024 <zj4204@gmail.com>
2025-05-22 02:58:07 +0800

    Update solve.py

--- lab8/solve.py
+++ lab8/solve.py
@@ -1,19 +1,25 @@
 import angr
 import sys
+import claripy
+
+angr.loggers.disable_root_logger()
 
 def main():
-    angr.loggers.disable_root_logger()
+    proj = angr.Project("chal", auto_load_libs=False)
+    flag_bytes = [claripy.BVS(f'byte_{i}', 8) for i in range(8)]
+    flag = claripy.Concat(*flag_bytes)
 
-    proj = angr.Project("chal")
-    state = proj.factory.entry_state(stdin=angr.SimFile)
+    state = proj.factory.full_init_state(
+        stdin = angr.SimFileStream(name='stdin', content=flag, has_end=True)
+    )
+
     simgr = proj.factory.simulation_manager(state)
     
-    simgr.explore(find=lambda s: b"flag" in s.posix.dumps(1),
-                 avoid=lambda s: b"Wrong key!" in s.posix.dumps(1))
+    simgr.explore(find=lambda s: b"flag" in s.posix.dumps(1))
 
     if len(simgr.found) > 0:
         found_state = simgr.found[0]
-        solution = found_state.posix.dumps(0)
+        solution = found_state.solver.eval(flag, cast_to=bytes)
         sys.stdout.buffer.write(solution)
     else:
         print("No solution found")
