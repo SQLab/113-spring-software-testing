74962e88a6eff42eebddc63594948833eeda7770
skysoul1024 <zj4204@gmail.com>
2025-05-22 02:47:37 +0800

    Update solve.py

--- lab8/solve.py
+++ lab8/solve.py
@@ -1,11 +1,22 @@
-#!/usr/bin/env python3
-
-import angr,sys
+import angr
+import sys
 
 def main():
-    secret_key = b""
-    sys.stdout.buffer.write(secret_key)
+    angr.loggers.disable_root_logger()
 
+    proj = angr.Project("chal")
+    state = proj.factory.entry_state(stdin=angr.SimFile)
+    simgr = proj.factory.simulation_manager(state)
+    
+    simgr.explore(find=lambda s: b"Correct!" in s.posix.dumps(1),
+                 avoid=lambda s: b"Wrong key!" in s.posix.dumps(1))
+
+    if len(simgr.found) > 0:
+        found_state = simgr.found[0]
+        solution = found_state.posix.dumps(0)
+        sys.stdout.buffer.write(solution)
+    else:
+        print("No solution found")
 
 if __name__ == '__main__':
     main()
